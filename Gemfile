cfceb0a22b68f943f1b7eade2dcfafb8d71bd938
evis-go <evis6174@gmail.com>
2018-04-20 22:11:33 +0300

    Added support for any query

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem "rails", "~> 4.2.8"
 gem "google-cloud-datastore", "~> 1.4"
+gem 'fast-stemmer'
 
 gem "jquery-rails"
 
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,8 +1,9 @@
+require 'fast_stemmer'
 require "google/cloud/datastore"
 
 class Search
-  
-  @index_kind = 'index_dev'
+
+  INDEX_KIND = 'index'
 
   # Google::Cloud::Datastore::Dataset for the configured dataset
   @@datastore ||= Google::Cloud::Datastore.new(
@@ -10,12 +11,50 @@ class Search
                       database_configuration[Rails.env]["dataset_id"]
   )
 
-  def self.query(query)
-    # get the current entity if it exists
-    entity_key = @@datastore.key @index_kind, query
-    entity = @@datastore.find(entity_key)
-    result_hash = entity == nil ? {} : eval(entity['value'])
-    result_hash.keys
+  def self.query(user_query)
+    query = simplify_query(user_query)
+
+    results = {}
+    query.each do |index|
+      results[index] = retrieve_index(index)
+    end
+
+    filtered_results = []
+    init_index = query[0]
+
+    results[init_index].each do |url, value|
+      f = true
+      query.each do |index|
+        f &&= results[index].key? url
+      end
+      filtered_results << url if f
+    end
+    filtered_results
+  end
+
+  private
+
+  def self.retrieve_index(index)
+    offset = 0
+    result_hash = {}
+    while true
+      key = @@datastore.key(INDEX_KIND, "#{index}#{offset}")
+      entity = @@datastore.find(key)
+      break if entity == nil
+      result_hash.merge!(eval(entity['value']))
+      offset += 1
+    end
+    result_hash
+  end
+
+  def self.simplify_query(user_query)
+    query = user_query.gsub(/[^a-z ]/i, ' ').split()
+    simple_query = []
+    query.each do |word|
+      token = Stemmer::stem_word(word.downcase)
+      simple_query << token if !simple_query.include? word
+    end
+    simple_query
   end
 
 end
