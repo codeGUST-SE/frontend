61f7a937dadab8aefea627ccc1cc63497ac97622
evis-go <evis6174@gmail.com>
2018-04-20 14:07:20 +0300

    Redirect to home page when query is empty

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
   def index
     @query = params.has_key?(:q) ? params[:q] : ''
+    redirect_to root_path if @query == ''
     @search_results = @query == '' ? [] : Search.query(@query)
   end
 
