34a57d424ce7798d286c0f2e85a0d2ba9d6f8cbd
evis-go <evis6174@gmail.com>
2018-04-25 21:19:07 +0300

    Changed score weights to floats and added log to count_score

--- lib/document_retrieval/document.rb
+++ lib/document_retrieval/document.rb
@@ -1,14 +1,14 @@
 class Document
 
-  W = { :order_score => 1, :title_score => 1, :count_score => 1 }
+  W = { :order_score => 1.0, :title_score => 1.0, :count_score => 1.0 }
 
   attr_accessor :order_score, :title_score, :count_score, :url
 
-  def initialize(url, order_score = 0, title_score = 0, count_score = 0)
+  def initialize(url, order_score = 0.0, title_score = 0.0, count_score = 0.0)
     @url = url
     @order_score = order_score
     @title_score = title_score
-    @count_score = count_score
+    @count_score = Math::log(count_score)
   end
 
   def total
