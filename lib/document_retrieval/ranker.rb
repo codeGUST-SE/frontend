9dd75040769b0ea753d0cc16d332139a2cb770ce
evis-go <evis6174@gmail.com>
2018-04-25 22:29:04 +0300

    Small refactoring

--- lib/document_retrieval/ranker.rb
+++ lib/document_retrieval/ranker.rb
@@ -8,11 +8,11 @@ class Ranker
     @query = query
   end
 
-  def get_ranked_documents(url_to_pos, index_to_url)
+  def get_ranked_documents(url_to_index_to_pos, index_to_url)
 
     # calculate title_score
     title_scores = {}
-    url_to_pos.each do |url, pos_list|
+    url_to_index_to_pos.each do |url, pos_list|
       title_scores[url] = 0
       @query.each do |index|
         title_scores[url] += index_to_url[index][url][0]
@@ -21,7 +21,7 @@ class Ranker
 
     # calculate count_score
     count_score = {}
-    url_to_pos.each do |url, pos_list|
+    url_to_index_to_pos.each do |url, pos_list|
       count_score[url] = 0
       @query.each do |index|
         count_score[url] += index_to_url[index][url][1].length
@@ -30,7 +30,7 @@ class Ranker
 
     # calculate order_score
     order_score = {}
-    url_to_pos.each do |url, pos_list|
+    url_to_index_to_pos.each do |url, pos_list|
       order_score[url] = 0
       pos_list.each_with_index do |w1, w1_i|
         w1.each do |w1_pos|
@@ -54,6 +54,7 @@ class Ranker
 
     results = []
     (0..MAX_RESULT_NUMBER).each do
+      break if doc_pq.top == nil
       results << doc_pq.pop
     end
 
