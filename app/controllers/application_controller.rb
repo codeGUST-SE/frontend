da68ceab6560e173407fcfe4974787fd4cb3a4be
evis-go <evis6174@gmail.com>
2018-05-09 18:42:53 +0200

    Fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@ class ApplicationController < ActionController::Base
   def home
   end
 
-  def mobile
-    puts params
-    @query = params.has_key?(:q) ? params[:q] : ''
-    redirect_to root_path if @query == ''
-    render json: @query == '' ? [] : Search.query(@query)
-  end
-
 end
